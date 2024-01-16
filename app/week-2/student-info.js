e3028fe64908cabbf80c52e67f45d157ea83151a
Ayesha <68371207+akhalil95@users.noreply.github.com>
2024-01-16 14:42:30 -0700

    week 2 assignment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,9 @@
+import Link from "next/link";
+export default function StudentInfo() {
+  return (
+    <main>
+      <h1>Ayesha Khalil</h1>
+      <Link href="https://github.com/akhalil95">https://github.com</Link>
+    </main>
+  );
+}
