c028c5ac6a6c3f2cb932f134727773dd39a6697f
Ayesha <68371207+akhalil95@users.noreply.github.com>
2024-02-28 12:04:29 -0700

    update page.js

--- app/page.js
+++ app/page.js
@@ -1,12 +1,28 @@
+import layout from "./layout";
 import Link from "next/link";
+
 export default function Page() {
   return (
-    <main>
-      <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <Link href="/week-2">Week 2</Link>
-      <Link href="/week-3">Week 3</Link>
-      <Link href="/week-4">Week 4</Link>
-      <Link href="/week-5">Week 5</Link>
+    <main className="bg-gradient-radial from-blue-500 to-purple-600 min-h-screen flex flex-col items-center py-10">
+      <h1 className="text-center text-3xl font-bold mb-8">
+        CPRG 306: Web Development 2 - Assignments
+      </h1>
+      <div className="flex flex-wrap justify-center gap-4">
+        <LinkCard href="/week-2" title="Week 2" />
+        <LinkCard href="/week-3" title="Week 3" />
+        <LinkCard href="/week-4" title="Week 4" />
+        <LinkCard href="/week-5" title="Week 5" />
+      </div>
     </main>
+  );
+}
+
+function LinkCard({ href, title }) {
+  return (
+    <Link href={href} passHref>
+      <div className="block p-6 w-80 bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 cursor-pointer text-gray-800 ">
+        {title}
+      </div>
+    </Link>
   );
 }
